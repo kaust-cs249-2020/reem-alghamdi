49fc26b8d38c60a5702abd61c339e59bcbe9d611
reem-codes <reem.brain@gmail.com>
2020-09-03 12:02:40 +0300

    ch1 part 8 and 11 + reorganization

---
+++ ch1/code/ch1_11.py
@@ -0,0 +1,56 @@
+"""
+@BY: Reem Alghamdi
+@DATE: 03-09-2020
+"""
+
+def immediate_neighbors(pattern):
+    """
+    This function takes a pattern, and return ALL patterns where the hamming distance is 1
+    :param pattern: the pattern to find its neighbors
+    :return: an array of patterns where the hamming distance between them and the pattern is 1
+    """
+    neighborhood = []
+    nucleotides = ["A", "C", "T", "G"]
+    for index in range(len(pattern)):
+        symbol = pattern[index]
+        for nucleotide in nucleotides:
+            if symbol != nucleotide:
+                neighbor = list(pattern)
+                neighbor[index] = nucleotide
+                neighbor = "".join(neighbor)
+                neighborhood.append(neighbor)
+    neighborhood.append(pattern)
+    print(*neighborhood)
+    return neighborhood
+
+
+def neighbors(pattern, d):
+    """
+    This function takes a pattern, and a hamming distance and return ALL patterns where the hamming distance is d
+    :param pattern: the pattern to find its neighbors
+    :param d: the maximum hamming distance between the pattern and the neighbors
+    :return: an array of patterns where the hamming distance between them and the pattern is at most d
+    """
+    from ch1.code.ch1_8 import hamming_distance
+
+    nucleotides = ["A", "C", "T", "G"]
+
+    if d == 0:
+        return [pattern]
+    if len(pattern) == 1:
+        return nucleotides
+    neighborhood = []
+    suffix_neighbors = neighbors(pattern[1:], d)
+    for text in suffix_neighbors:
+        if hamming_distance(text, pattern[1:]) < d:
+            for nucleotide in nucleotides:
+                neighborhood.append(nucleotide + text)
+        else:
+            neighborhood.append(pattern[0] + text)
+    return neighborhood
+
+
+if __name__ == "__main__":
+    immediate_neighbors("ACG")
+    print(*neighbors("TGTAGATATAA", 3))
+    print(len(neighbors("TGTAGATATAA", 3)))
---
+++ ch1/code/ch1_2.py
@@ -0,0 +1,72 @@
+"""
+@BY: Reem Alghamdi
+@DATE: 01-09-2020
+"""
+
+
+def pattern_count(text, pattern):
+    """
+    This function takes a pattern and count how many times it appeared inside a bigger string
+    :param text: the full string
+    :param pattern: the pattern to count in string
+    :return: number of occurrence of pattern in string
+    """
+    count = 0
+    for index in range(len(text) - len(pattern) + 1):
+        kmer = text[index: index + len(pattern)]
+        if kmer == pattern:
+            count = count + 1
+    return count
+
+
+def frequency_table(text, k):
+    """
+    This function takes a text and the length of the k-mers. Then construct a dictionary of ALL k-mers along their occurrences
+
+    :param text: the full string
+    :param k: the length of each substring needed to build k-mers
+    :return: a dictionary where the keys are the k-mers and the value how many times they appeared
+    """
+
+    table = {}
+    for index in range(len(text) - k + 1):
+        kmer = text[index: index + k]
+        if table.get(kmer):
+            table[kmer] = table[kmer] + 1
+        else:
+            table[kmer] = 1
+    return table
+
+
+def max_map(dictionary):
+    """
+    This function takes a dictionary of keys and positive values, then return the max value
+    :param dictionary: the dictionary. ex: frequency table
+    :return: the max value
+    """
+    _max = 0
+    for key, value in dictionary.items():
+        if value > _max:
+            _max = value
+    return _max
+
+
+def better_frequent_words(text, k):
+    """
+    This function takes a string and a pattern length, and return the patterns with highest occurrence
+    :param text: the text
+    :param k: the k-mer length
+    :return: list of k-mers of most occurrence
+    """
+    frequent_pattern = []
+    freq_map = frequency_table(text, k)
+    _max = max_map(freq_map)
+    for pattern, freq in freq_map.items():
+        if freq == _max:
+            frequent_pattern.append(pattern)
+    return frequent_pattern
+
+
+if __name__ == "__main__":
+    print(pattern_count("TTATGTGCTACGTGTGCTATGTGCTATGTGCTAGGCCCGGCTGTGCTACATGTGCTATGTGCTATTTTAGAGTGTGCTAATGTGCTACTGTGCTACCGTGTGCTACTGTGCTATGTGCTAACGACAGTGTGCTAATGATGTGCTATGTGCTATTTCCAGTGTGCTAGTGTGCTACGTCTGTGCTATGTGCTATGTGCTATGTGCTATGTCGTGTGCTATGTGCTAATGTGCTACGCTAGGTTGTGCTAGTCTGTGCTAAGTCTTTGTGCTACGTAGATGTGCTACTGAACCGTATGTGCTACTGTGCTACGTATGTGCTAAGCGTGTGCTACATGTGCTAATGTGCTATGTGCTAGTCGTTGTGCTATTCTGTGCTATGTGCTATGTGCTAGTGTGCTAGTGTGCTATGTGCTACTTGTGCTATTGTGCTAGGGTTTGTGCTAATTGTCGATGTGCTAGTGTGCTAATGTGCTAATGTGCTATGTGCTATGTGCTACTGTGCTAATGTGCTATGTGGCAATTGTGCTATGTGCTAGCATGTGCTATCGAGCTCATGTGCTAACCGGATTGTGCTATGTGCTAATATGTGCTAAGATGCTGTGCTATGTGTGCTATGTGCTAGCCAGGGGTGTGCTATTGTGCTAGTGATCTGCTGTCATGTGCTAGGTTGTGCTACGATGTGCTAAACTCGTGTGCTAACTTGTGCTATTGTGCTATGTGCTACGTGTGCTAGATGTGCTAAAGCAAGTGTGCTATGTGCTATTGTGCTATTTCTGCCGTGTGCTATGCTGCATGTGCTATGTGCTAATGTGCTACAGTGTGCTATTGTGCTAGTGTGCTATGTGCTATGTGCTATGTGCTAAATGTGCTATGTGCTAGAATGTGCTATGTGCTATGTGCTAAACACCTGTGCTATGTGCTATTGTGCTAAATCTAGCCTGTGCTATTATTATGTGCTAACTGTGCTAAGGTGG", "TGTGCTATG"))
+    print(*better_frequent_words("ACGAGGAGAAAAAAGCCCTCACAAGTAAACGAGGAGAATATGTTTAACCTGACCTTTCTGACCTTTTATGTTTAACCTGACCTTTCTGACCTTTCTGACCTTTACAAGTAATATGTTTAACACAAGTAAACGAGGAGAAAAAAGCCCTCCTGACCTTTAAAAGCCCTCTATGTTTAACAAAAGCCCTCTATGTTTAACAAAAGCCCTCTATGTTTAACTATGTTTAACACAAGTAAAAAAGCCCTCACAAGTAACTGACCTTTCTGACCTTTTATGTTTAACACAAGTAAAAAAGCCCTCACGAGGAGAACTGACCTTTACGAGGAGAACTGACCTTTACGAGGAGAATATGTTTAACAAAAGCCCTCCTGACCTTTACAAGTAAACGAGGAGAAAAAAGCCCTCAAAAGCCCTCACGAGGAGAAACGAGGAGAAACGAGGAGAAACAAGTAAACGAGGAGAAAAAAGCCCTCTATGTTTAACCTGACCTTTACGAGGAGAACTGACCTTTTATGTTTAACTATGTTTAACCTGACCTTTCTGACCTTTCTGACCTTTAAAAGCCCTCACGAGGAGAAAAAAGCCCTCCTGACCTTTACAAGTAACTGACCTTTCTGACCTTTACGAGGAGAACTGACCTTTACGAGGAGAAACAAGTAAACAAGTAATATGTTTAACAAAAGCCCTCCTGACCTTTCTGACCTTTACAAGTAAACAAGTAACTGACCTTTTATGTTTAACTATGTTTAACACGAGGAGAAAAAAGCCCTCAAAAGCCCTCCTGACCTTTACAAGTAACTGACCTTTACAAGTAACTGACCTTTTATGTTTAACCTGACCTTTACGAGGAGAACTGACCTTTCTGACCTTTCTGACCTTTACAAGTAAACGAGGAGAA", 12))
